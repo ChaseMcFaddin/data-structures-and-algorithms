e11a898f7f67e4a481eeb7e2ab0d9e49b56142b5
Chase McFaddin <comcfadd@gmail.com>
2021-03-25 17:05:56 -0700

    challenge one working with Ceylin

---
+++ java/code-challenges/src/main/java/ArrayReverse.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class reverseArray {
+  public static void reverse (String[] args){
+    int[] a = new int[n];
+    int[] b = n;
+    for(int i = 0; i < arr.length; i++){
+      a[b - 1] = c[j];
+      b = b - 1; 
+    }
+      System.out.println(n);
+
+    
+
+  }
+  public static void main (String[] args){
+  int[] arr = {1,2,3,4,5};
+  reverse(arr, arr.length);
+
+  }
+  
+}
+
